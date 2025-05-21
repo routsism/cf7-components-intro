b7c2dd9a70d0b0e5acd0b1dac45387444b76c592
Routsis Michail <mike7199@icloud.com>
2025-05-21 20:01:14 +0300

    feat: added layout component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,15 @@
+const Footer = () => {
+   const currentYear: number = new Date().getFullYear();
+   
+    return (
+        <>
+            <footer className="bg-gray-700">
+                <div className="text-white text-center py-4">
+                    Copyright © {currentYear}, Coding Factory 7. All rights reserved.
+                </div>
+            </footer>
+        </>
+    )
+}
+
+export default Footer;
