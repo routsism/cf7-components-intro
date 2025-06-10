5a1477db6c57eeb4920dd992cd46efa7449f69cc
Routsis Michail <mike7199@icloud.com>
2025-06-10 20:19:14 +0300

    feat: focus input example

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@
 // import OnlineStatus from "./components/OnlineStatus.tsx";
 // import ClassComponentWithState from "./components/ClassComponentWithState.tsx";
 import {BrowserRouter, Routes, Route} from "react-router";
-import  HomePage  from "./pages/HomePage.tsx";
+// import  HomePage  from "./pages/HomePage.tsx";
 import NameChangerPage from "./pages/NameChangerPage.tsx";
 import OnlineStatusPage from "./pages/OnlineStatusPage.tsx";
 import UserPage from "./pages/UserPage.tsx";
@@ -27,6 +27,7 @@ import ExamplesPage from "./pages/ExamplesPage.tsx";
 import RouterExamplesLayout from "./components/RouterExamplesLayout.tsx";
 import AutoRedirectPage from "./pages/AutoRedirectPage.tsx";
 import NotFoundPage from "./pages/NotFoundPage.tsx";
+import FocusInput from "./components/FocusInput.tsx";
 
 // useEffect(() => {
 //     const id: number = setInterval(() => console.log("tick"),1000)
@@ -70,7 +71,8 @@ function App() {
                     {/*<Route path="/" element={<HomePage/>}/>*/}
 
                     <Route element={<RouterLayout />}>
-                        <Route index element={<HomePage/>}/>
+                        {/*<Route index element={<HomePage/>}/>*/}
+                        <Route index element={<FocusInput/>}/>
                         <Route path="users/:userId" element={<UserPage/>}/>
                         <Route path="users" element={<UserPage />}/>
                     </Route>
