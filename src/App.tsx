d57d39e67ecc839cfba5ca26605da61d9e04d3f6
Routsis Michail <mike7199@icloud.com>
2025-06-11 20:37:07 +0300

    feat: add error messages after input elements

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,8 @@ import AutoRedirectPage from "./pages/AutoRedirectPage.tsx";
 import NotFoundPage from "./pages/NotFoundPage.tsx";
 // import FocusInput from "./components/FocusInput.tsx";
 // import UncontrolledInput from "./components/UncontrolledInput.tsx";
-import MultiFieldForm from "./components/MultiFieldForm.tsx";
+// import MultiFieldForm from "./components/MultiFieldForm.tsx";
+import MultiFieldFormWithValidation from "./components/MultiFieldFormWithValidation.tsx";
 
 // useEffect(() => {
 //     const id: number = setInterval(() => console.log("tick"),1000)
@@ -76,7 +77,8 @@ function App() {
                         {/*<Route index element={<HomePage/>}/>*/}
                         {/*<Route index element={<FocusInput/>}/>*/}
                         {/*<Route index element={<UncontrolledInput/>}/>*/}
-                        <Route index element={<MultiFieldForm/>}/>
+                        {/*<Route index element={<MultiFieldForm/>}/>*/}
+                        <Route index element={<MultiFieldFormWithValidation/>}/>
                         <Route path="users/:userId" element={<UserPage/>}/>
                         <Route path="users" element={<UserPage />}/>
                     </Route>
