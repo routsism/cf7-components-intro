93e57d7d5f4593c835fab7aa75921d7b3a95c628
Routsis Michail <mike7199@icloud.com>
2025-06-16 19:49:58 +0300

    feat: MultiFieldForm component

--- src/components/MultiFieldForm.tsx
+++ src/components/MultiFieldForm.tsx
@@ -1,9 +1,9 @@
 import {useState} from "react";
 
 type FormValues = {
-    name: string,
-    email: string,
-    message: string,
+    name: string;
+    email: string;
+    message: string;
 };
 
 const initialValues = {
@@ -25,13 +25,10 @@ const MultiFieldForm = () => {
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
         const {name, value} = e.target;
-
-        setValues(
-            (prev) => ({
-              ...prev,
-              [name]: value,
-            }));
-
+        setValues (prev => ({
+            ...prev,
+            [name]: value,
+        }));
     };
 
     const handleClear = () => {
@@ -39,64 +36,62 @@ const MultiFieldForm = () => {
         setSubmittedData(null);
     };
 
-    return (
+    return(
         <>
-            <div className=" flex text-center max-w-sm mx-auto mt-8" >
-            <form onSubmit={handleSubmit} className="mx-auto max-w-md text-center mt-8 space-y-4">
-                <input
-                    type="text"
-                    name="name"
-                    value={values.name}
-                    placeholder="Name"
-                    onChange={handleChange}
-                    className="w-full px-4 py-2 rounded border"
-                    required
-                />
-                <input
-                    type="email"
-                    name="email"
-                    value={values.email}
-                    placeholder="Email"
-                    onChange={handleChange}
-                    className="w-full px-4 py-2 rounded border"
-                    required
-                />
-                <textarea
-                    name="message"
-                    placeholder="Type your message"
-                    value={values.message}
-                    onChange={handleChange}
-                    className="w-full px-4 py-2 rounded border"
-                    minLength={5}
-                    required
-                ></textarea>
-                <div className="flex gap-4">
-                    <button
-                        type="submit"
-                        className="bg-cf-dark-red text-white px-4 py-2 rounded"
-                    >
-                        Submit
-                    </button>
-                    <button
-                        type="button"
-                        onClick={handleClear}
-                        className="bg-gray-200 text-cf-gray px-4 py-2 rounded">
-                        Clear
-                    </button>
-                </div>
+            <div className="flex max-w-sm mx-auto mt-8">
+                <form onSubmit={handleSubmit} className="space-y-4">
+                    <input
+                        type="text"
+                        name="name"
+                        value={values.name}
+                        placeholder="Name"
+                        onChange={handleChange}
+                        className="w-full px-4 py-2 rounded border"
+                        required
+                    />
+                    <input
+                        type="email"
+                        name="email"
+                        value={values.email}
+                        placeholder="Email"
+                        onChange={handleChange}
+                        className="w-full px-4 py-2 rounded border"
+                        required
+                    />
+                    <textarea
+                        name="message"
+                        value={values.message}
+                        placeholder="Type your message"
+                        onChange={handleChange}
+                        className="w-full px-4 py-2 rounded border"
+                        minLength={5}
+                        required
+                    ></textarea>
+                    <div className="flex gap-4">
+                        <button
+                            type="submit"
+                            className="bg-cf-dark-red text-white px-4 py-2 rounded"
+                        >
+                            Submit
+                        </button>
+                        <button
+                            type="button"
+                            onClick={handleClear}
+                            className="bg-gray-200 text-cf-gray-700 px-4 py-2 rounded">
+                            Clear
+                        </button>
+                    </div>
 
-                { submittedData && (
-                <div className="mt-6 border-t pt-4 space-y-2">
-                    <h2 className="font-semibold">Submitted Data</h2>
-                    <p><strong>Name: </strong> {submittedData.name}</p>
-                    <p><strong>Email: </strong> {submittedData.email}</p>
-                    <p><strong>Message: </strong> {submittedData.message}</p>
-                </div>
-                )}
+                    { submittedData && (
+                        <div className="mt-6 border-t pt-4 space-y-2">
+                            <h2 className="font-semibold">Submitted Data</h2>
+                            <p><strong>Name:</strong> {submittedData.name}</p>
+                            <p><strong>Email:</strong> {submittedData.email}</p>
+                            <p><strong>Message:</strong> {submittedData.message}</p>
+                        </div>
+                    )}
 
-
-
-            </form>
+                </form>
             </div>
         </>
     )
