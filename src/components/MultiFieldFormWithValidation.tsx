0c78a92a2bf69483e260d60f901044d9b608bdb6
Routsis Michail <mike7199@icloud.com>
2025-06-16 19:49:47 +0300

    feat: MultiFieldFormWithZodValidation component

--- src/components/MultiFieldFormWithValidation.tsx
+++ src/components/MultiFieldFormWithValidation.tsx
@@ -1,15 +1,15 @@
 import {useState} from "react";
 
 type FormValues = {
-    name: string,
-    email: string,
-    message: string,
+    name: string;
+    email: string;
+    message: string;
 };
 
 type FormErrors = {
-    name?: string,
-    email?: string,
-    message?: string,
+    name?: string;
+    email?: string;
+    message?: string;
 };
 
 const initialValues = {
@@ -25,28 +25,30 @@ const MultiFieldFormWithValidation = () => {
 
     const validateForm = (values: FormValues): FormErrors => {
         const errors: FormErrors = {};
-        if (!values.name.trim()) {
-          errors.name = "Name is required.";
+
+        if (!values.name.trim()){
+            errors.name = "Name is required."
         }
 
         if (!values.email.trim() ||
             !/^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/.
-            test(values.email.trim())) {
-            errors.email = "Email is required.";
+            test(values.email.trim()) ){
+            errors.email = "Email is required."
         }
 
-        if (values.message.length < 5) {
-            errors.message = "Message is required.";
+        if (values.message.length < 5){
+            errors.message = "Message is required."
         }
+
         return errors;
-    };
 
+
+    };
 
     const handleSubmit = (e: React.FormEvent) => {
         e.preventDefault();
-
-        const validationErrors =  validateForm(values);
-        if (Object.keys(validationErrors).length > 0) {
+        const validationErrors = validateForm(values);
+        if (Object.keys(validationErrors).length > 0){
             setErrors(validationErrors);
             setSubmittedData(null);
             return;
@@ -58,14 +60,12 @@ const MultiFieldFormWithValidation = () => {
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
         const {name, value} = e.target;
-        setValues(
-            (prev) => ({
-              ...prev,
-              [name]: value,
-            }));
-
+        setValues (prev => ({
+            ...prev,
+            [name]: value,
+        }));
         setErrors(
-            (prev) => ({
+            prev => ({
                 ...prev,
                 [name]: undefined,
             }));
@@ -73,80 +73,76 @@ const MultiFieldFormWithValidation = () => {
 
     const handleClear = () => {
         setValues(initialValues);
-        setSubmittedData(null);
         setErrors(null);
+        setSubmittedData(null);
     };
 
-    return (
+    return(
         <>
-            <div className=" flex text-center max-w-sm mx-auto mt-8" >
-            <form onSubmit={handleSubmit} className="mx-auto max-w-md text-center mt-8 space-y-4">
-                <div>
-                    <input
-                        type="text"
-                        name="name"
-                        value={values.name}
-                        placeholder="Name"
-                        onChange={handleChange}
-                        className="w-full px-4 py-2 rounded border"
-                    />
-                    {errors?.name && (
-                        <p className="text-cf-dark-red">{errors?.name}</p>
+            <div className="flex max-w-sm mx-auto mt-8">
+                <form onSubmit={handleSubmit} className="space-y-4">
+                    <div>
+                        <input
+                            type="text"
+                            name="name"
+                            value={values.name}
+                            placeholder="Name"
+                            onChange={handleChange}
+                            className="w-full px-4 py-2 rounded border"
+                        />
+                        {errors?.name && (
+                            <p className="text-cf-dark-red">{errors.name}</p>
+                        )}
+                    </div>
+                    <div>
+                        <input
+                            type="text"
+                            name="email"
+                            value={values.email}
+                            placeholder="Email"
+                            onChange={handleChange}
+                            className="w-full px-4 py-2 rounded border"
+                        />
+                        {errors?.email && (
+                            <p className="text-cf-dark-red">{errors.email}</p>
+                        )}
+                    </div>
+                    <div>
+            <textarea
+                name="message"
+                value={values.message}
+                placeholder="Type your message"
+                onChange={handleChange}
+                className="w-full px-4 py-2 rounded border"
+                minLength={5}
+            ></textarea>
+                        {errors?.message && (
+                            <p className="text-cf-dark-red">{errors.message}</p>
+                        )}
+                    </div>
+                    <div className="flex gap-4">
+                        <button
+                            type="submit"
+                            className="bg-cf-dark-red text-white px-4 py-2 rounded"
+                        >
+                            Submit
+                        </button>
+                        <button
+                            type="button"
+                            onClick={handleClear}
+                            className="bg-gray-200 text-cf-gray-700 px-4 py-2 rounded">
+                            Clear
+                        </button>
+                    </div>
+                    { submittedData && (
+                        <div className="mt-6 border-t pt-4 space-y-2">
+                            <h2 className="font-semibold">Submitted Data</h2>
+                            <p><strong>Name:</strong> {submittedData.name}</p>
+                            <p><strong>Email:</strong> {submittedData.email}</p>
+                            <p><strong>Message:</strong> {submittedData.message}</p>
+                        </div>
                     )}
-                </div>
-                <div>
-                    <input
-                        type="text"
-                        name="email"
-                        value={values.email}
-                        placeholder="Email"
-                        onChange={handleChange}
-                        className="w-full px-4 py-2 rounded border"
-                    />
-                    {errors?.email && (
-                        <p className="text-cf-dark-red">{errors?.email}</p>
-                    )}
-                </div>
-                <div>
-                    <textarea
-                        name="message"
-                        placeholder="Type your message"
-                        value={values.message}
-                        onChange={handleChange}
-                        className="w-full px-4 py-2 rounded border"
-                        minLength={5}
-                    ></textarea>
-                    {errors?.message && (
-                        <p className="text-cf-dark-red">{errors?.message}</p>
-                    )}
-                </div>
-                <div className="flex gap-4">
-                    <button
-                        type="submit"
-                        className="bg-cf-dark-red text-white px-4 py-2 rounded"
-                    >
-                        Submit
-                    </button>
-                    <button
-                        type="button"
-                        onClick={handleClear}
-                        className="bg-gray-200 text-cf-gray px-4 py-2 rounded">
-                        Clear
-                    </button>
-                </div>
-
-                { submittedData && (
-                <div className="mt-6 border-t pt-4 space-y-2">
-                    <h2 className="font-semibold">Submitted Data</h2>
-                    <p><strong>Name: </strong> {submittedData.name}</p>
-                    <p><strong>Email: </strong> {submittedData.email}</p>
-                    <p><strong>Message: </strong> {submittedData.message}</p>
-                </div>
-                )}
-
-
-
-            </form>
+                </form>
             </div>
         </>
     )
